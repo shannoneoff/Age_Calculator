a3d7998ed12266f0384bd6168bd0edb5f6af5d0b
shannoneoff <shannon.eoff@gmail.com>
2023-03-25 17:40:51 -0500

    Customize look, add math function to calc months

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,24 @@
 // CALCULATES AGE
-function ageCalculator(date) {
+// Finds years result
+function yearsCalculator(date) {
+  const current = new Date();
+  const difference = Math.abs(current - date);  // Finds difference between birthdate and current date
+  const years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365)); // Rounds down to nearest integer
+                                                                      // to find number of years
+  return years
+}
+
+// Finds months result
+function monthsCalculator(date) {
   const current = new Date();
   const difference = Math.abs(current - date);
-  const age = Math.floor(difference / (1000 * 60 * 60 * 24 * 365));
-  return age
+  const years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365));
+  const decYears = Math.abs(difference / (1000 * 60 * 60 * 24 * 365));  // Finds absolute value, no rounding
+  const remainder = (decYears - years).toFixed(2);  // Once years are calculated, this finds a two-decimal
+                                                    // remainder for us to use to find months
+  const findMonths = Math.floor(12 * remainder);  // Takes remainder and finds how many months it equals,
+                                                  // then rounds down to nearest integer
+  return findMonths;  
 }
 
 // PIKADAY CALENDAR
@@ -11,7 +26,9 @@ var picker = new Pikaday({
   field: document.getElementById('datepicker'),
   yearRange: [1900, 2023],  // Will need update for end-range year from time to time
   onSelect: function (date) {
-    let result = ageCalculator(date);
-    document.getElementById('result').innerHTML = "<mark>" + result + "</mark>";
+    let years = yearsCalculator(date);
+    let months = monthsCalculator(date);
+    document.getElementById('years').innerHTML = years + " years, ";  // Sends results to index.html
+    document.getElementById('months').innerHTML = months + " months";
   }
 });
--- js/reset.js
+++ js/reset.js
@@ -1,3 +1,6 @@
+// RESETS AGE RESULTS
+// HTML reset button by default only resets the input, so we needed to also reset age result
 function resetForm() {
-  document.getElementById('result').innerHTML = '';
+  document.getElementById('years').innerHTML = '';  // Hides years result
+  document.getElementById('months').innerHTML = ''; // Hides months result
 }
